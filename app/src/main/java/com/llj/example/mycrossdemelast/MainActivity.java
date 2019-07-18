142affe93cfd80cd8f7a2f4af470853be8c1e003
lilujunx <@163.com>
2019-07-18 09:35:44 +0800

    The first fit

---
+++ app/src/main/java/com/llj/example/mycrossdemelast/MainActivity.java
@@ -0,0 +1,40 @@
+package com.llj.example.mycrossdemelast;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    private MyTest mMyTest;
+    private EditText mEdt;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mEdt = (EditText) findViewById(R.id.edt);
+
+
+
+
+        new Button(this).setOnClickListener(l -> {
+            Toast.makeText(this, "lalala", Toast.LENGTH_SHORT).show();
+        });
+
+
+
+    }
+
+    public void setTest(MyTest myTest) {
+        this.mMyTest = myTest;
+    }
+
+
+    interface MyTest {
+        void xx(int x, int y, int z, String old);
+    }
+}
